18ae9353a9e17acc78506822e80cd9a8c0ff777b
MateusCamana <m_camana@hotmail.com>
2021-05-27 16:30:16 -0300

    Script organization

--- R/S.R
+++ R/S.R
@@ -1,7 +1,8 @@
-####Carregando o pacote para o gráfico####
+###Carregando pacotes###
 
 require(corrplot)
-
+library(here)
+library(pivot)
 ###Gerando e exportando o gráfico####
 
 #Criando o caminho o arquivo -> CONFERIR O PAR
@@ -13,7 +14,17 @@ corrplot(cor(clima[,c(3:8,11)],use="complete.obs"),type="upper", method="ellipse
 dev.off()
 
 ###Gerando e exportando a matriz com alguns dados##
-# Aqui vou exportar algumas colunas da matriz "clima"
+matriz_correl <-(cor(clima[,c(3:8,11)]))
+
+write.table (matriz_correl, file = (here("output", "tables","correl.csv")),
+             sep = ",", quote = TRUE)
+
 
 ###Gerando e exportando um SM##
-# Aqui vou exportar os dados de bioma e o ID
+bioma <- (unique(clima[,c(10,11)]))
+unique (bioma)
+write.table (bioma, file = (here("output","supp","biome_ID.csv")),
+             sep = ",", quote = TRUE)
+
+
+
